8b35bdf048d2d042898020ac72919a1be2510c82
Son Pham Hong <phamhongson1107@gmail.com>
2023-11-09 21:42:33 +0700

    make login

---
+++ Gitdemo/Controller/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  Gitdemo
+//
+//  Created by Phạm Hồng Sơn on 09/11/2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    func login(){
+        print("make login")
+    }
+}
